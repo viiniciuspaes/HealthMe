94bd994de658b65b031cf059aeb15e00aab4c220
João D'Amorim <joao.damorim@gmail.com>
2017-07-26 17:48:12 -0300

    Criação da SplashActivity e Xml, modificações no Manifest.

---
+++ app/src/main/java/usuario/gui/SplashActivity.java
@@ -0,0 +1,25 @@
+package usuario.gui;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends Activity implements Runnable {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Handler handler = new Handler();
+        handler.postDelayed(this, 3000);
+    }
+
+    public void run(){
+        startActivity(new Intent(this,LogInActivity.class));
+        finish();
+    }
+}
+
+
