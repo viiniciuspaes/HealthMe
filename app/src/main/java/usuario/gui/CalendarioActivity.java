6da088fbedbd00bba2c297ff78d2d3da918a1770
João D'Amorim <joao.damorim@gmail.com>
2017-07-31 20:23:48 -0300

    ListView Eventos completo e funcionado. Alterações na activity e xml.

--- app/src/main/java/usuario/gui/CalendarioActivity.java
+++ app/src/main/java/usuario/gui/CalendarioActivity.java
@@ -1,10 +1,13 @@
 package usuario.gui;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.CalendarView;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 
 import usuario.dao.ContatoDao;
 import usuario.dao.EventoDao;
@@ -13,8 +16,13 @@ import usuario.negocio.SessaoUsuario;
 
 public class CalendarioActivity extends AppCompatActivity {
     private CalendarView calendario;
-    private EventoDao eventoDao;
+    private EventoDao daoEvento;
     private SessaoUsuario sessaoUsuario;
+    private ContatoDao daoContato;
+
+    ListView listViewContatos;
+    Cursor  cursor;
+    SimpleCursorAdapter adpter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,7 +43,19 @@ public class CalendarioActivity extends AppCompatActivity {
             }
         });
         sessaoUsuario = new SessaoUsuario(getApplicationContext());
-        eventoDao = new EventoDao(getApplicationContext());
+        daoEvento = new EventoDao(getApplicationContext());
         sessaoUsuario.iniciarSessao();
+
+        setTextos();
+    }
+    public void setTextos(){
+
+        Cursor c = daoEvento.buscarDados(sessaoUsuario.getUsuarioLogado().getLogin());
+        String[] from = new String[]{"_id","evento_nome","evento_usuario","descricao","data"};
+        int[] to = new int[]{R.id.txvEventoId,R.id.txvEventoNome,R.id.txvEventoUsuario,R.id.txvEventoDescricao,R.id.txvEventoData};
+        adpter = new SimpleCursorAdapter(getApplicationContext(), R.layout.modelo_listview_eventos, c, from, to,0);
+        adpter.notifyDataSetChanged();
+        listViewContatos = (ListView) findViewById(R.id.listaDeContatos);
+        listViewContatos.setAdapter(adpter);
     }
 }
