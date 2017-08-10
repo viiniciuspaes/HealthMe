c128b571eb4b60f2bb4217fb198837e2fbebcaa8
João D'Amorim <joao.damorim@gmail.com>
2017-08-10 20:20:18 -0300

    Botão de pânico 100% funcional na TelaInicialNavActivity.

--- app/src/main/java/usuario/gui/TelaInicialNavActivity.java
+++ app/src/main/java/usuario/gui/TelaInicialNavActivity.java
@@ -1,11 +1,18 @@
 package usuario.gui;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.telephony.SmsManager;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -16,7 +23,14 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import usuario.dao.ContatoDao;
+import usuario.dominio.ContatoEmergencia;
+import usuario.negocio.ContatoNegocio;
 import usuario.negocio.SessaoUsuario;
 
 public class TelaInicialNavActivity extends AppCompatActivity
@@ -25,6 +39,7 @@ public class TelaInicialNavActivity extends AppCompatActivity
     private SharedPreferences preferences;
     private SessaoUsuario sessao;
     private TextView boasVindas;
+    private ContatoNegocio negocioContato;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -103,5 +118,73 @@ public class TelaInicialNavActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+    public void panico(View v)throws Exception {
+
+        sessao = new SessaoUsuario(getApplicationContext());
+        negocioContato = new ContatoNegocio(getApplicationContext());
+        sessao.iniciarSessao();
+
+        for(ContatoEmergencia x: negocioContato.smsContato(sessao)) {
+            SmsManager smsManager = SmsManager.getDefault();
+            smsManager.sendTextMessage(x.getNumero(), null, "Socorro by HealthMe", null, null);
+        }
+
+        if (isPermissionGranted()) {
+            call_action();
+        }
+    }
+    public void call_action(){
+        String phnum = "996556828";
+        Intent callIntent = new Intent(Intent.ACTION_CALL).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        //check wheather it is dual sim or not then
+
+        //if sim 1
+        callIntent.putExtra("com.android.phone.extra.slot",0);
+
+        //else if sim 2
+        //callIntent.putExtra("simSlot", 1);
+
+        callIntent.setData(Uri.parse("tel:" + phnum));
+        startActivity(callIntent);
+    }
+    public  boolean isPermissionGranted() {
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (checkSelfPermission(android.Manifest.permission.CALL_PHONE)
+                    == PackageManager.PERMISSION_GRANTED) {
+                Log.v("TAG","Permission is granted");
+                return true;
+            } else {
+
+                Log.v("TAG","Permission is revoked");
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CALL_PHONE}, 1);
+                return false;
+            }
+        }
+        else { //permission is automatically granted on sdk<23 upon installation
+            Log.v("TAG","Permission is granted");
+            return true;
+        }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+
+            case 1: {
+
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(getApplicationContext(), "Permission granted", Toast.LENGTH_SHORT).show();
+                    call_action();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Permission denied", Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
     }
 }
--- app/src/main/java/usuario/negocio/ContatoNegocio.java
+++ app/src/main/java/usuario/negocio/ContatoNegocio.java
@@ -5,11 +5,15 @@ import android.content.Context;
 import android.database.Cursor;
 import android.widget.SimpleCursorAdapter;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import usuario.dao.ContatoDao;
+import usuario.dominio.ContatoEmergencia;
 import usuario.gui.R;
+import usuario.gui.TelaInicialNavActivity;
 
 public class ContatoNegocio {
     private Context context;
@@ -17,6 +21,7 @@ public class ContatoNegocio {
     private Matcher m;
     private ContatoDao daoContato;
     private SimpleCursorAdapter adapter;
+    private SessaoUsuario sessaoUsuario;
 
     public ContatoNegocio(Context context) {
         this.context = context;
@@ -28,6 +33,7 @@ public class ContatoNegocio {
         m = p2.matcher(texto);
         return m.matches();
     }
+
     public SimpleCursorAdapter construtorAdapter(SessaoUsuario sessaoUsuario){
         Cursor c = daoContato.buscarDados(sessaoUsuario.getUsuarioLogado().getLogin());
         String[] from = new String[]{"_id","contato_usuario","contato_nome","contato_telefone"};
@@ -36,4 +42,13 @@ public class ContatoNegocio {
         adapter.notifyDataSetChanged();
         return adapter;
     }
+
+    public ArrayList<ContatoEmergencia> smsContato(SessaoUsuario sessao){
+        ArrayList<ContatoEmergencia> meusContatos = (ArrayList<ContatoEmergencia>) daoContato.buscarContatos(sessao.getUsuarioLogado().getLogin());
+        return meusContatos;
+    }
 }
+
+
+
+
