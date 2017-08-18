aba5b60e974344b2c7e4eb5ff8d4d2508afd2166
João D'Amorim <joao.damorim@gmail.com>
2017-08-17 22:20:49 -0300

    Método probabilidade completo.

--- app/src/main/java/usuario/gui/ResultadosExamesActivity.java
+++ app/src/main/java/usuario/gui/ResultadosExamesActivity.java
@@ -27,33 +27,64 @@ public class ResultadosExamesActivity extends AppCompatActivity {
         arrayRespostas =getIntent().getExtras().getStringArrayList("lista");
         redeBayesiana = new MinhaRedeBayesiana();
 
-
         ListView lista = (ListView) findViewById(R.id.lvResultados);
-        //resultados.add(probabilidade());
-        //resultados.add(probabilidade());
-
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, probabilidade());
         lista.setAdapter(arrayAdapter);
-
-       // ArrayList<String> respostas = ResultadosExamesActivity.respostas;
-
-        //ArrayList<String> dados = new ArrayList<String>();
-
 
     }
     public ArrayList<String> probabilidade(){
 
         ArrayList<String> dados = new ArrayList<>();
-
         if(arrayRespostas.get(0).equals("sim") && arrayRespostas.get(1).equals("sim")){
             dados.add("Cardiologia");
             dados.add(redeBayesiana.a_e_b());
-
-
+        }
+        if(arrayRespostas.get(0).equals("sim") && arrayRespostas.get(1).equals("nao")){
+            dados.add("Cardiologia");
+            dados.add(redeBayesiana.a_e_nb());
+        }
+        if(arrayRespostas.get(0).equals("nao") && arrayRespostas.get(1).equals("sim")){
+            dados.add("Cardiologia");
+            dados.add(redeBayesiana.na_e_b());
+        }
+        if(arrayRespostas.get(0).equals("nao") && arrayRespostas.get(1).equals("nao")){
+            dados.add("Cardiologia");
+            dados.add(redeBayesiana.na_e_nb());
+        }
+        if(arrayRespostas.get(2).equals("sim") && arrayRespostas.get(3).equals("sim")){
+            dados.add("Otorrinolaringologia");
+            dados.add(redeBayesiana.c_e_d());
+        }
+        if(arrayRespostas.get(2).equals("sim") && arrayRespostas.get(3).equals("nao")){
+            dados.add("Otorrinolaringologia");
+            dados.add(redeBayesiana.c_e_nd());
+        }
+        if(arrayRespostas.get(2).equals("nao") && arrayRespostas.get(3).equals("sim")){
+            dados.add("Otorrinolaringologia");
+            dados.add(redeBayesiana.nc_e_d());
+        }
+        if(arrayRespostas.get(2).equals("nao") && arrayRespostas.get(3).equals("nao")){
+            dados.add("Otorrinolaringologia");
+            dados.add(redeBayesiana.nc_e_nd());
+        }
+        if(arrayRespostas.get(4).equals("sim") && arrayRespostas.get(5).equals("sim")){
+            dados.add("Oftalmologia");
+            dados.add(redeBayesiana.e_e_f());
+        }
+        if(arrayRespostas.get(4).equals("sim") && arrayRespostas.get(5).equals("nao")){
+            dados.add("Oftalmologia");
+            dados.add(redeBayesiana.e_e_nf());
+        }
+        if(arrayRespostas.get(4).equals("nao") && arrayRespostas.get(5).equals("sim")){
+            dados.add("Oftalmologia");
+            dados.add(redeBayesiana.ne_e_f());
+        }
+        if(arrayRespostas.get(4).equals("nao") && arrayRespostas.get(5).equals("nao")){
+            dados.add("Oftalmologia");
+            dados.add(redeBayesiana.ne_e_nf());
         }
         return dados;
     }
-
     public void voltarParaHome(View view) throws Exception{
        startActivity(new Intent(ResultadosExamesActivity.this, TelaInicialNavActivity.class));
    }
